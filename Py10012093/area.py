445025264fc3ed5051030ca3d7f9e8d6153553d3
alarnu2012 <56617396+alarnu2012@users.noreply.github.com>
2019-10-15 21:11:35 -0500

    Add files via upload

---
+++ Py10012093/area.py
@@ -0,0 +1,10 @@
+base = int(input('Ingrese base : '))
+altura = int(input('Ingrese altura : '))
+area = (base * altura)
+print ('El Area del Retangulo de base : ' + str(base) + ' por altura : ' + str(altura) + ' es : '  + str(area))
+Cadena,numero, lista = "Hola", 12345, [1,2,3,5]
+
+print (Cadena)
+print (str(numero))
+print (lista)
+
---
+++ Py10012093/holamundo.py
@@ -0,0 +1,10 @@
+#Este es mi primer ejercicio en PYTHON.
+nombre = input("Ingrese su nombre :")
+print ("Hola Mundo en python : " + nombre)
+'''Preubeade comentario
+   por bloque
+'''
+
+
+
+
